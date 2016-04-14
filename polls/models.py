a0a3a805df58091297c99b5164ba29136af4a5e8
Anthony Mills <millzilla@users.noreply.github.com>
2016-04-14 00:07:39 -0700

    Added was_published_recently bool method

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
+  """Model representing a Question."""
   def __str__(self):
     return self.question_text
 
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
+  """Model representing a question Choice."""
   def __str__(self):
     return self.choice_text
 
