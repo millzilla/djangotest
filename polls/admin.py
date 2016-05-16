4221e5d36480e39a36de45b37df6cb2441e5641f
Anthony Mills <millzilla@users.noreply.github.com>
2016-05-15 18:23:43 -0700

    Django Tutorial pt. 2; tweaked & improved Question admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+  """Customization for inline Choice options."""
+  model = Choice
+  extra = 1  # Provide 1 extra blank Choice by default
+
+
+class QuestionAdmin(admin.ModelAdmin):
+  """Customization for the Question admin page."""
+  # Control the ordering of the Question properties
+  # fields = ['pub_date', 'question_text']
+
+  # Control both the ordering and the grouping of the Question properties
+  fieldsets = [
+    (None,               {'fields': ['question_text']}),
+    ('Date information', {'fields': ['pub_date']})
+  ]
+
+  # Also allow us to modify Choices inline
+  inlines = [ChoiceInline]
+
+  # Show more columns than just the question text on the list page
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+  # Show filtering options - use pub_date
+  list_filter = ['pub_date']
+
+  # Add a search box over the question_text field
+  search_fields = ['question_text']
+
+# Register the Question admin site with the QuestionAdmin options
+admin.site.register(Question, QuestionAdmin)
+
+# Register the Choice admin site
+# This allows you to add individual choices and associate them with an existing
+# or a new Question; not the optimal way to do this
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -3,24 +3,35 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Question(models.Model):
   """Model representing a Question."""
   def __str__(self):
     return self.question_text
 
+  # Question properties
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
+  # Question instance methods
   def was_published_recently(self):
     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+  # Additional properties that permit sorting/filtering on Admin page
+  # 'admin_order_field': associate the method with a particular property
+  was_published_recently.admin_order_field = 'pub_date'
+  # 'boolean': use a pretty icon instead of T/F
+  was_published_recently.boolean = True
+  # 'short_description': list column heading
+  was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
   """Model representing a question Choice."""
   def __str__(self):
     return self.choice_text
 
+  # Choice properties
   question = models.ForeignKey(Question)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
